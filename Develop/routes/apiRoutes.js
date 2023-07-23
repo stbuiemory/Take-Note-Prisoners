d0ad154e815ae8ff71590801f8d98ef24e45045e
Sandy Bui <stbui_emory@yahoo.com>
2023-07-23 19:12:37 -0400

    testing

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,10 +1,6 @@
 const fs = require('fs');
 
 module.exports = function (app) {
-
-    // The application should have a db.json file on the backend that will be used to store and retrieve notes using the fs module.
-
-    // GET /api/notes - Should read the db.json file and return all saved notes as JSON.
     app.get("/api/notes", function (req, res) {
         console.log("Getting Notes");
         fs.readFile("db/db.json", "utf8", (err, data) => {
@@ -14,7 +10,6 @@ module.exports = function (app) {
         });
     });
 
-    // POST /api/notes - Should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
     app.post("/api/notes", function (req, res) {
         fs.readFile("db/db.json", "utf8", (err, data) => {
             if (err) throw err;
@@ -36,13 +31,12 @@ module.exports = function (app) {
         });
     });
 
-    // DELETE /api/notes/:id - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique id when it's saved. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
     app.delete("/api/notes/:id", function (req, res) {
         fs.readFile("db/db.json", "utf8", (err, data) => {
             if (err) throw err;
 
             let notes = JSON.parse(data);
-            // let notes = JSON.parse(notesData);
+           
             let notesId = req.params.id;
             let newNotesId = 0;
 
